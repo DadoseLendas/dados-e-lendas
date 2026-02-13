e1ce5614a2604d48ba61dd07dae5d02e5b7a7d08
Felipe Gabriel <felipe.g.silva2018@gmail.com>
2026-02-13 15:00:29 -0400

    Alteração função middleware para proxy para manter a concordância.

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
@@ -38,7 +38,6 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  // Atualiza a sessão
   await supabase.auth.getUser()
 
   return response
@@ -46,13 +45,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public (public files like logo.png)
-     */
     '/((?!_next/static|_next/image|favicon.ico|logo.png|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
